3a548fb4ba23949903094f351146c4f4c55f2e4e
FRANK <frankmascco@gmail.com>
2025-05-30 11:51:05 -0500

    add order-service

---
+++ order-service/src/main/java/com/frank/microservices/order/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.frank.microservices.order.service;
+
+import com.frank.microservices.order.dto.OrderRequest;
+import com.frank.microservices.order.model.Order;
+import com.frank.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+
+        orderRepository.save(order);
+    }
+}
