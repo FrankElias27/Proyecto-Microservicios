61dab2f0a9011d69d6757c888f50c57936089cec
FRANK <frankmascco@gmail.com>
2025-05-30 23:37:09 -0500

    feat: add communication between order and inventory microservices

--- order-service/src/main/java/com/frank/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/frank/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.frank.microservices.order.service;
 
+import com.frank.microservices.order.client.InventoryClient;
 import com.frank.microservices.order.dto.OrderRequest;
 import com.frank.microservices.order.model.Order;
 import com.frank.microservices.order.repository.OrderRepository;
@@ -13,15 +14,25 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
+         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
 
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+         if(isProductInStock){
+             Order order = new Order();
+             order.setOrderNumber(UUID.randomUUID().toString());
+             order.setPrice(orderRequest.price());
+             order.setSkuCode(orderRequest.skuCode());
+             order.setQuantity(orderRequest.quantity());
 
-        orderRepository.save(order);
+             orderRepository.save(order);
+         }else{
+             throw new RuntimeException("Product with SkuCode "+orderRequest.skuCode()+" is not in stock");
+         }
+
+
     }
+
+
 }
